a132c6a79f962c08302c70c6a3d1c5990e817518
BB20101997 <bennet-blessmann@t-online.de>
2019-01-06 02:18:25 +0100

    pdflatex needs to be run at least twice to produce correct results

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,7 @@ set -ve
 
 cd poster
 pdflatex -interaction=nonstopmode -halt-on-error *.tex -aux-directory="build"
+pdflatex -interaction=nonstopmode -halt-on-error *.tex -aux-directory="build"
 cd ..
 
 cp poster/*.pdf pages
